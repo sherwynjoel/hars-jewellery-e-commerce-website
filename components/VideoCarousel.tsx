7745436049b6c8ccaf49c8907fbe32d2914fa248
Sherwyn Joel <sherwynjoel@example.com>
2025-11-24 17:36:27 +0530

    chore: hide video carousel dots on mobile

--- components/VideoCarousel.tsx
+++ components/VideoCarousel.tsx
@@ -302,7 +302,7 @@ export default function VideoCarousel() {
 
             {/* Dots Indicator */}
             {visibleItems.length > 1 && (
-              <div className="absolute bottom-4 left-1/2 -translate-x-1/2 flex items-center gap-2 z-50">
+              <div className="hidden sm:flex absolute bottom-4 left-1/2 -translate-x-1/2 items-center gap-2 z-50">
                 {visibleItems.map((_, index) => (
                   <button
                     key={index}
